0e1668e28ee8887c9cd78e2cb070722c2e04ac2f
Les Moffat <les.moffat@maptiler.com>
2025-11-04 11:58:57 +0000

    it works

--- e2e/tests/basic.test.ts
+++ e2e/tests/basic.test.ts
@@ -2,7 +2,13 @@ import { expect, test } from "@playwright/test";
 
 import loadFixtureAndGetMapHandle from "./helpers/loadFixtureAndGetMapHandle";
 
-test("Loads and renders a simple mesh", async ({ page }, testInfo) => {
+test("does stuff with the page", async ({ page }) => {
+   loadFixtureAndGetMapHandle({
+    fixture: "basic",
+    page,
+   });
 
+   await expect(page.locator('h1')).toHaveText('Hello, world!');
+   await expect(page).toHaveScreenshot('basic.png');
 });
 
