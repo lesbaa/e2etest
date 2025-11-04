ffd1e8d1ebc72e8a1ddc3a2609d8e6e8736c2e81
Les Moffat <les.moffat@maptiler.com>
2025-11-04 13:29:39 +0000

    ljklkjlkj

--- vite.config-e2e.ts
+++ vite.config-e2e.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path'; // Make sure to import 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        basic: 'public/basic.html',
+        basic: path.resolve(__dirname, 'e2e/public/basic.html'),
       },
     },
   },
-  root: 'e2e',
+  root: './e2e',
 });
