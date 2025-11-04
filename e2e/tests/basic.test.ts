15252de2f351c8cb6188fefb5b6f7a8043a82300
Les Moffat <les.moffat@maptiler.com>
2025-11-04 12:06:11 +0000

    fix test

--- e2e/tests/basic.test.ts
+++ e2e/tests/basic.test.ts
@@ -3,12 +3,11 @@ import { expect, test } from "@playwright/test";
 import loadFixtureAndGetMapHandle from "./helpers/loadFixtureAndGetMapHandle";
 
 test("does stuff with the page", async ({ page }) => {
-   loadFixtureAndGetMapHandle({
+   await loadFixtureAndGetMapHandle({
     fixture: "basic",
     page,
    });
 
-   await expect(page.locator('h1')).toHaveText('Hello, world!');
    await expect(page).toHaveScreenshot('basic.png');
 });
 
